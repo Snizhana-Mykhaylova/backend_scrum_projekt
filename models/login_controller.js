3607fc60d3404a7c5eee3da7407590f8c40d6d8d
KenanSalar <kenansalar@outlook.com>
2024-02-22 11:14:12 +0100

    documentation

--- models/login_controller.js
+++ models/login_controller.js
@@ -1,23 +1,30 @@
+/**
+ * @module login_controller
+ * @description Dieses Modul enthält Funktionen für die Authentifizierung und Benutzerregistrierung.
+ */
+
+// Erforderliche Module importieren
 const express = require("express");
 const pool = require("./VSS_DatabaseConnect");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+// SQL-Abfragen definieren
+const getUser = "SELECT * FROM user_login WHERE user_name =$1 AND user_password = $2";
+const addUser = "INSERT INTO user_login (user_name, user_password) VALUES ($1,$2)";
+const getPassword = "SELECT user_password FROM user_login WHERE user_name=$1 LIMIT 1";
 
-const getUser =
-  "SELECT * FROM user_login WHERE user_name =$1 AND user_password = $2";
-const addUser =
-  "INSERT INTO user_login (user_name, user_password) VALUES ($1,$2)";
-const getPassword =
-  "SELECT user_password FROM user_login WHERE user_name=$1 LIMIT 1";
-
+/**
+ * @function getUserLogin
+ * @description Überprüft die Benutzeranmeldeinformationen und gibt eine entsprechende Nachricht zurück.
+ * @param {Object} req - Express Request-Objekt
+ * @param {Object} res - Express Response-Objekt
+ */
 const getUserLogin = async (req, res) => {
   try {
     const { user_name, user_password } = req.body;
-    console.log(user_name);
-    console.log(user_password);
 
-    pool.query(queries.getUser, async (error, results) => {
+    pool.query(getUser, async (error, results) => {
       results.rows.forEach((element) => {
         const row = Object.values(element);
         const username = row[0];
@@ -25,9 +32,8 @@ const getUserLogin = async (req, res) => {
         const isValid = bcrypt.compareSync(user_password, password);
 
         if (username && isValid) {
-          console.log("Username und Password sind richtig");
-          return res.json({ Message: "Username und Password sind richtig" });
-          //   return res.sendFile(path.join(__dirname, '../views/MainComponent.js'));
+          console.log("Username und Password sind korrekt");
+          return res.json({ Message: "Username und Password sind korrekt" });
         }
 
         if (!username && isValid) {
@@ -42,35 +48,42 @@ const getUserLogin = async (req, res) => {
       });
     });
   } catch (error) {
-    console.error("error", error);
+    console.error("Fehler", error);
   }
 };
 
+/**
+ * @function checkUndAddUsersingup
+ * @description Überprüft die Benutzerregistrierung und fügt einen neuen Benutzer hinzu, wenn der Benutzername noch nicht existiert.
+ * @param {Object} req - Express Request-Objekt
+ * @param {Object} res - Express Response-Objekt
+ */
 const checkUndAddUsersingup = async (req, res) => {
   try {
     const { user_name, user_password } = req.body;
 
-    pool.query(queries.getUser, (error, results) => {
+    pool.query(getUser, (error, results) => {
       if (results.rows.length) {
-        return res.send("Username is already exist");
+        return res.send("Benutzername existiert bereits");
       } else {
-        const hashpassword = bcrypt.hash(user_password, saltRounds);
+        const hashpassword = bcrypt.hashSync(user_password, saltRounds);
 
         pool.query(
-          queries.addUser,
+          addUser,
           [user_name, hashpassword],
           (error, result) => {
             if (error) throw error;
-            res.status(200).send("ok");
+            res.status(200).send("Erfolgreich registriert");
           }
         );
       }
     });
   } catch (error) {
-    console.error("error", error);
+    console.error("Fehler", error);
   }
 };
 
+// Module exportieren
 module.exports = {
   getUserLogin,
   checkUndAddUsersingup,
